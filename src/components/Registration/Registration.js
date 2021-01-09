f2b9d421b1d773d33d39666d3335b47ff2b4a479
alexandrprokhorov1988 <alexandrprokhorov1988@gmail.com>
2021-01-10 01:48:31 +0300

    feat: добавить css registration

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import './Registration.css';
+import {useFormValidation} from '../../hooks/useFormValidation';
 
 function Registration() {
+  const { values, handleChange, errors, isValid, resetForm } = useFormValidation();
+
+  React.useEffect(() => {
+    resetForm();
+  }, [resetForm]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!values.name || !values.phone) {
+      return;
+    }
+    resetForm();
+  }
+
   return (
     <section className="registration">
       <div className="registration__container">
@@ -10,6 +25,51 @@ function Registration() {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat enim tortor in hac id imperdiet adipiscing.
           Pellentesque nisi, mi sit non sit sed fermentum.
         </p>
+        <form action="#" method="post" className="registration__form" noValidate onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="registration__input registration__input_type_name"
+            name="name"
+            autoComplete="on"
+            minLength="2"
+            maxLength="200"
+            required
+            placeholder="Имя"
+            value={values.name || ''}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            className="registration__input registration__input_type_phone"
+            name="phone"
+            autoComplete="on"
+            minLength="2"
+            maxLength="200"
+            required
+            placeholder="Телефон"
+            value={values.phone || ''}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            className="registration__input registration__input_type_address"
+            name="address"
+            autoComplete="on"
+            minLength="2"
+            maxLength="200"
+            required
+            placeholder="Телефон"
+            value={values.address || ''}
+            onChange={handleChange}
+          />
+          <input
+            type="submit"
+            className={`registration__submit-button ${!isValid ? 'registration__submit-button_inactive' : ''}`}
+            name="submit"
+            value="Записаться"
+            disabled={!isValid}
+          />
+        </form>
       </div>
     </section>
   )
