9ec7b0e19aad566bac245b7b5f6fbf54053e8664
alexandrprokhorov1988 <alexandrprokhorov1988@gmail.com>
2021-01-09 19:03:31 +0300

    feat: registration

---
+++ src/components/Registration/Registration.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Registration.css';
+
+function Registration() {
+  return (
+    <section className="registration">
+      <div className="registration__container">
+        <h2 className="registration__title">Запись на дегустацию</h2>
+        <p className="registration__subtitle">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat enim tortor in hac id imperdiet adipiscing.
+          Pellentesque nisi, mi sit non sit sed fermentum.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Registration;
